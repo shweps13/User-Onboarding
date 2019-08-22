eb17bdeea3d41127e4db37c61c524f01c1fa586a
Heorhii Siburov <g.siburov@gmail.com>
2019-08-22 14:30:55 -0700

    Yup validation was added

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,23 +1,40 @@
 import React from "react";
 import { Form, Field, withFormik } from "formik";
+import * as Yup from "yup";
 
-const HtmlForm = ({ values }) => {
+const HtmlForm = ({ values, errors, touched }) => {
  
     return (
       <div className="onboarding-form">
           <h2>Onboarding Form</h2>
           <Form className="in-form">
             <Field type="text" name="username" placeholder="Full name here" />
+            {touched.username && errors.username && (
+            <p className="error">{errors.username}</p>
+            )}
+
             <Field type="email" name="email" placeholder="Email here" />
+            {touched.email && errors.email && (
+            <p className="error">{errors.email}</p>
+            )}
+
             <Field type="password" name="passwd" placeholder="Enter your password" />
+            {touched.passwd && errors.passwd && (
+            <p className="error">{errors.passwd}</p>
+            )}
+
             <Field component="select" className="acc-select" name="acctype">
                 <option>Please choose your account type</option>
                 <option value="permanent">Permanent</option>
                 <option value="temporary">Temporary</option>
             </Field>
+
             <label className="checkbox-container">
                 <p>Accept Terms of Service</p>
                 <Field type="checkbox" name="terms" checked={values.terms} />
+                {touched.terms && errors.terms && (
+                <p className="error">{errors.terms}</p>
+                )}
             </label>
             <button type="submit">Submit!</button>
           </Form>
@@ -37,6 +54,26 @@ const FormikHtmlForm = withFormik ({
           terms: terms || false
         };
       },
+
+    validationSchema: Yup.object().shape({
+        username: Yup.string()
+        .required("Username is required*"),
+        email: Yup.string()
+        .email('E-mail is not valid!')
+        .required("Email is required*"),
+        passwd: Yup.string()
+        .required("Password is required*")
+        .min(3, 'Password has to be longer than 3 characters!'), 
+        terms: Yup.bool()
+        .test(
+        'terms',
+        'You have to agree with our Terms and Conditions!',
+        value => value === true
+      )
+      .required(
+        'You have to agree with our Terms and Conditions!'
+      ),
+    }),
 
 })(HtmlForm);
 
