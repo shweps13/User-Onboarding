d91618868c69ad45a4df374cb5d8122a7b280510
Heorhii Siburov <g.siburov@gmail.com>
2019-08-22 13:10:02 -0700

    form added, need some styling

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,13 +1,25 @@
 import React from "react";
 import { Form, Field, withFormik } from "formik";
 
-const HtmlForm = () => {
+const HtmlForm = ({ values }) => {
  
     return (
       <div className="onboarding-form">
           <h2>Onboarding Form</h2>
           <Form>
-            <Field type="text" name="username" placeholder="The name of user" />
+            <Field type="text" name="username" placeholder="Full name here" />
+            <Field type="text" name="email" placeholder="Email here" />
+            <Field type="text" name="passwd" placeholder="Enter your password" />
+            <Field component="select" className="acc-select" name="acctype">
+                <option>Please choose your account type</option>
+                <option value="permanent">Permanent</option>
+                <option value="temporary">Temporary</option>
+            </Field>
+            <label className="checkbox-container">
+                Terms of Service
+                <Field type="checkbox" name="terms" checked={values.terms} />
+            </label>
+            <button type="submit">Submit!</button>
           </Form>
 
       </div>
@@ -16,9 +28,13 @@ const HtmlForm = () => {
   
 const FormikHtmlForm = withFormik ({
 
-    mapPropsToValues({ username }) {
+    mapPropsToValues({ username, email, passwd, terms, acctype }) {
         return {
-          username: username || ""
+          username: username || "",
+          email: email || "",
+          passwd: passwd || "",
+          acctype: acctype || "",
+          terms: terms || false
         };
       },
 
